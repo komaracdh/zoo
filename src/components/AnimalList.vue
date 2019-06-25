4796bc9580d5929b0c45e698282a490f9ddd0aee
Milos <komaracdh@gmail.com>
2019-06-25 19:20:16 +0200

    zadatak tri

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -2,6 +2,7 @@
  <ul>
    <li v-for="(animal, index) in animals" :key="index" >
      {{animal.type}}  {{animal.name}}  {{animal.dateOfBirth ? animal.dateOfBirth : "Nepoznat"}}
+    <button @click="deleteAnimal(index)" > Remove animal </button>
    </li>
  </ul>
 </template>
@@ -17,6 +18,12 @@ data(){
       {type:"Mouse", name:"Mickey",dateOfBirth:"11/11/1931"},
       {type:"Turtle", name:"Leonardo",dateOfBirth:""}
     ]
+  }
+},
+
+methods: {
+  deleteAnimal(index){
+    this.animals.splice(index,1)
   }
 }
 }
